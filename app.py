4b3cf37db834d955b066da4d68a7c8ceab696d22
Michael Holloway <mholloway@wikimedia.org>
2019-07-17 15:04:42 -0400

    Add config file and use it to configure logging level

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from aiohttp import web
 import logging
 from PIL import ImageFile
+import yaml
 from lib.app import NSFWApp
 
-# TODO: Get log level from service config
-logging.basicConfig(level='INFO')
+with open('config.yaml') as config_definition:
+    config = yaml.safe_load(config_definition)
+    if config is None or config['logging'] is None or config['logging']['level'] is None:
+        log_level = 'WARNING'
+    else:
+        log_level = config['logging']['level'].upper()
+
+logging.basicConfig(level=log_level)
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
